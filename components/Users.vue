b9231f571cb78064d3e61676fbdef35b1277e92c
Joshua Oluikpe <joshuaoluikpe@Joshuas-MacBook-Pro-2.local>
2022-04-19 00:42:58 +0100

    Add background color

--- components/Users.vue
+++ components/Users.vue
@@ -26,9 +26,10 @@
 <style>
 .user-container {
   display: flex;
-  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.12), 0px 2px 2px rgba(0, 0, 0, 0.24);
-  font-family: "Roboto";
   margin-top: 21px;
+  background: #fafafa;
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.12), 0px 2px 2px rgba(0, 0, 0, 0.24);
+  border-radius: 3px;
 }
 
 .user-container img {
